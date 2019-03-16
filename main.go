56264bcc5ee216e106ee2e438775505c009d89db
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-16 07:00:53 +0000

    Defining our models (structs)

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
+
+type Post struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+var posts []Post
 
 func main() {
 	router := mux.NewRouter()
@@ -11,4 +21,5 @@ func main() {
 	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
 	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
 	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+	http.ListenAndServe(":8000", router)
 }
