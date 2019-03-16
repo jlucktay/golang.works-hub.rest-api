2df05ff97b61afc9ca09e7a88a4cb33d1d7ff951
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-16 07:00:11 +0000

    Creating our endpoints

--- main.go
+++ main.go
@@ -6,4 +6,9 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", createPost).Methods("POST")
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
 }
