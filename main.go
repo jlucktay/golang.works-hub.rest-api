fbadc67264c74d4ed4be30c77ca08f0ae813f069
James Lucktaylor <jlucktay+github@gmail.com>
2019-03-16 07:07:09 +0000

    Fin

--- main.go
+++ main.go
@@ -28,9 +28,8 @@ func getPost(w http.ResponseWriter, r *http.Request) {
 	for _, item := range posts {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
-			break
+			return
 		}
-		return
 	}
 	json.NewEncoder(w).Encode(&Post{})
 }
@@ -38,7 +37,7 @@ func getPost(w http.ResponseWriter, r *http.Request) {
 func createPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var post Post
-	_ = json.NewDecoder(r.Body).Decode(post)
+	_ = json.NewDecoder(r.Body).Decode(&post)
 	post.ID = strconv.Itoa(rand.Intn(1000000))
 	posts = append(posts, post)
 	json.NewEncoder(w).Encode(&post)
@@ -51,7 +50,7 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 		if item.ID == params["id"] {
 			posts = append(posts[:index], posts[index+1:]...)
 			var post Post
-			_ = json.NewDecoder(r.Body).Decode(post)
+			_ = json.NewDecoder(r.Body).Decode(&post)
 			post.ID = params["id"]
 			posts = append(posts, post)
 			json.NewEncoder(w).Encode(&post)
@@ -64,19 +63,18 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 func deletePost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _, item := range posts {
+	for index, item := range posts {
 		if item.ID == params["id"] {
-			posts = append(posts[:index], posts[index+1]...)
+			posts = append(posts[:index], posts[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(posts)
 }
 
 func main() {
 	router := mux.NewRouter()
 	posts = append(posts, Post{ID: "1", Title: "My first post", Body: "This is the content of my first post"})
-	posts = append(posts, Post{ID: "2", Title: "My second post", Body: "This is the content of my second post"})
 	router.HandleFunc("/posts", getPosts).Methods("GET")
 	router.HandleFunc("/posts", createPost).Methods("POST")
 	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
